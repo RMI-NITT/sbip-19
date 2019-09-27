6e5498debddf7376f74ee5744d56e0a2e5780839
Koushik <koushikkumaran52@gmail.com>
2019-09-28 00:53:19 +0530

    added acceleration to ball KF

--- src/getballdata_f.py
+++ src/getballdata_f.py
@@ -32,24 +32,28 @@ class image_converter:
   def __init__(self):
     self.image_pub = rospy.Publisher("ballmask",Image, queue_size = 10)
     self.kf_ballpub = rospy.Publisher("kf_ballpose",Pose, queue_size = 10)
-    self.kf_ballpub2 = rospy.Publisher("kf_balltwist", Twist, queue_size = 10)
+    self.kf_ballpub2 = rospy.Publisher("kf_ball_velocity", Twist, queue_size = 10)
+    self.kf_ballpub3 = rospy.Publisher("kf_ball_acceleration", Twist, queue_size = 10)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/fieldroi",Image,self.callback)
     self.timesub = rospy.Subscriber("/usb_cam/camera_info", CameraInfo, self.camcb)
     self.t_flag = 0
     self.init_flag = 0
-    self.X = np.matrix([[0],[0]], dtype = float)
-    self.Y = np.matrix([[0],[0]], dtype = float)
-    self.Px = np.eye(2, dtype = float)
-    self.Py = np.eye(2, dtype = float)
-    self.Q = np.eye(2, dtype = float)
-    self.Q[1][1] = 100
-    self.H = np.matrix([1,0], dtype = float)
-    self.R = np.eye(1, dtype = float)
-
+    self.X = np.matrix([[0],[0], [0]], dtype = float)
+    self.Y = np.matrix([[0],[0], [0]], dtype = float)
+    self.Px = np.eye(3, dtype = float)
+    self.Py = np.eye(3, dtype = float)
+    self.Q = 0.01*np.eye(3, dtype = float)
+    self.H = np.eye(3, dtype = float)
+    self.R = np.eye(3, dtype = float)
+    self.Zx = np.matrix([[0],[0], [0]], dtype = float)
+    self.Zy = np.matrix([[0],[0], [0]], dtype = float)
+    self.del_t = 0.067
+  
   def camcb(self, msg):
     if self.t_flag == 0:
         self.t0 = msg.header.seq
+        self.t_s = self.t0
         self.t_flag = 1
     self.t = msg.header.seq - self.t0
 
@@ -73,47 +77,53 @@ class image_converter:
       bp.position.y = cY
       
     fbp = Pose()  
-    fbt = Twist()
+    fbv = Twist()
+    fba = Twist()
     if self.init_flag == 0:
 
-        self.X[0] = bp.position.x
-        self.Y[0] = bp.position.y
+        self.Zx[0] = bp.position.x
+        self.Zy[0] = bp.position.y
         fbp.position.x = bp.position.x
         fbp.position.y = bp.position.y
         self.init_flag = 1
     else:
-        self.A = np.matrix([[1, self.t],[0, 1]], dtype=float)
-        self.X = np.matmul(self.A,self.X)
-        self.Y = np.matmul(self.A,self.Y)
+        print('working')
+        self.A = np.matrix([[1, self.del_t, self.del_t*self.del_t],[0, 1, self.del_t], [0, 0, 1]], dtype=float)
+        
+        self.X_prev = self.X
+        self.X = np.matmul(self.A,self.X_prev)
         self.Px = np.matmul(np.matmul(self.A,self.Px),np.transpose(self.A)) + self.Q
-        self.Py = np.matmul(np.matmul(self.A,self.Py),np.transpose(self.A)) + self.Q
+        self.Zx[0] = bp.position.x
+        self.Zx[1] = (self.Zx[0] - self.X_prev[0])/self.del_t
+        self.Zx[2] = (self.Zx[1] - self.X_prev[1])/self.del_t
 
         self.gain_x = np.matmul(np.matmul(self.Px, np.transpose(self.H)), np.linalg.inv(np.matmul(np.matmul(self.H,self.Px),np.transpose(self.H)) + self.R))
-        self.gain_y = np.matmul(np.matmul(self.Py, np.transpose(self.H)), np.linalg.inv(np.matmul(np.matmul(self.H,self.Py),np.transpose(self.H)) + self.R))
-        self.X = self.X + np.matmul(self.gain_x, (np.matrix([bp.position.x], dtype = float) - np.matmul(self.H, self.X)))
-        self.Y = self.Y + np.matmul(self.gain_y, (np.matrix([bp.position.y], dtype = float) - np.matmul(self.H, self.Y)))
-        self.Px = np.matmul((np.eye(2,dtype=float)-np.matmul(self.gain_x,self.H)),self.Px)
-        self.Py = np.matmul((np.eye(2,dtype=float)-np.matmul(self.gain_y,self.H)),self.Py)
+        self.X = self.X + np.matmul(self.gain_x, (self.Zx - np.matmul(self.H, self.X)))
+        self.Px = np.matmul((np.eye(3,dtype=float)- np.matmul(self.gain_x,self.H)),self.Px)
         
-        print(self.X.item(1),self.Y.item(1))
-        fbp.position.x = int(self.X.item(0))
-        fbp.position.y = int(self.Y.item(0))
-        fbt.linear.x = int(self.X.item(1))
-        fbt.linear.y = int(self.Y.item(1))
-    '''
-        self.state = np.matmul(self.A, self.state)
-        self.P = np.matmul(np.matmul(self.A,self.P),np.transpose(self.A)) + self.Q
+        self.Y_prev = self.Y
+        self.Y = np.matmul(self.A,self.Y_prev)
+        self.Py = np.matmul(np.matmul(self.A,self.Py),np.transpose(self.A)) + self.Q
+        self.Zy[0] = bp.position.y        
+        self.Zy[1] = (self.Zy[0] - self.Y_prev[0])/self.del_t
+        self.Zy[2] = (self.Zy[1] - self.Y_prev[1])/self.del_t
 
-        self.H = np.matrix([][], dtype = float)
-        self.gain = np.matmul(np.matmul(self.P, self.H), np.linalg.inv(np.matmul(np.matmul(self.H,self.P),np.transpose(self.H)) + self.R))
-        self.state = self.state + np.matmul(self.gain, np.matrix([bp.position.x, bp.position.y], dtype = float) - np.matmul(self.H,self.state))
-        fbp.position.x = state(0,0)z
-        fbp.position.y = state(0,1)
-    '''
+        self.gain_y = np.matmul(np.matmul(self.Py, np.transpose(self.H)), np.linalg.inv(np.matmul(np.matmul(self.H,self.Py),np.transpose(self.H)) + self.R))
+        self.Y = self.Y + np.matmul(self.gain_y, (self.Zy - np.matmul(self.H, self.Y)))
+        self.Py = np.matmul((np.eye(3,dtype=float)-np.matmul(self.gain_y,self.H)),self.Py)
+        
+        fbp.position.x = float(self.X.item(0))
+        fbp.position.y = float(self.Y.item(0))
+        fbv.linear.x = float(self.X.item(1))
+        fbv.linear.y = float(self.Y.item(1))
+        fba.linear.x = float(self.X.item(2))
+        fba.linear.y = float(self.Y.item(2))
+        
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(mask_white, "mono8"))
       self.kf_ballpub.publish(fbp)
-      self.kf_ballpub2.publish(fbt)
+      self.kf_ballpub2.publish(fbv)
+      self.kf_ballpub3.publish(fba)
     except CvBridgeError as e:
       print(e)
 
