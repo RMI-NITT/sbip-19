c113e2e2a659e8691baa1f202cda1fd542dee3f3
Koushik <koushikkumaran52@gmail.com>
2019-10-13 19:28:01 +0530

    split state estimator and stop point prediction

--- src/getballdata_f.py
+++ src/getballdata_f.py
@@ -11,12 +11,12 @@ roslib.load_manifest('sbip')
 import sys
 import rospy
 import cv2
-from std_msgs.msg import String
+from std_msgs.msg import String, Float64
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import Pose
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose, Twist
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import CameraInfo
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
 
 lower_white = np.array([(230,230,230)])
 higher_white = np.array([(255,255,255)])
@@ -31,33 +31,38 @@ def ftransform(x,y):
 class image_converter:
 
   def __init__(self):
- #   self.image_pub = rospy.Publisher("ballmask",Image, queue_size = 10)
     self.kf_ballpub = rospy.Publisher("kf_ballpose",Pose, queue_size = 10)
     self.kf_ballpub2 = rospy.Publisher("kf_ball_velocity", Twist, queue_size = 10)
     self.kf_ballpub3 = rospy.Publisher("kf_ball_acceleration", Twist, queue_size = 10)
     self.kf_ballpub4 = rospy.Publisher("kf_ball_stop", Pose, queue_size = 10)
+    self.Px_pub = rospy.Publisher("ball_Px", numpy_msg(Floats), queue_size=10)
+    self.Py_pub = rospy.Publisher("ball_Py", numpy_msg(Floats), queue_size=10)
     self.bridge = CvBridge()
-    self.image_pub = rospy.Publisher("ball_prediction",Image, queue_size = 10)
     self.image_sub = rospy.Subscriber("/fieldroi",Image,self.callback)
-    self.t_flag = 0
+    self.time_sub = rospy.Subscriber("/Time", Float64, self.time_cb)
     self.init_flag = 0
-    self.X = np.matrix([[0],[0], [0]], dtype = float)
-    self.Y = np.matrix([[0],[0], [0]], dtype = float)
-    self.Px = np.eye(3, dtype = float)
-    self.Py = np.eye(3, dtype = float)
-    self.Q = np.eye(3, dtype = float)
+    self.X = np.matrix([[0],[0], [0]], dtype = np.float32)
+    self.Y = np.matrix([[0],[0], [0]], dtype = np.float32)
+    self.Px = np.eye(3, dtype = np.float32)
+    self.Py = np.eye(3, dtype = np.float32)
+    self.Q = np.eye(3, dtype = np.float32)
     self.Q[0,0] = 1
-    self.Q[1,1] = 50
-    self.Q[2,2] = 200
-    self.H = np.matrix([[1, 0, 0],[0, 1, 0]], dtype = float)
-    self.R = np.eye(2, dtype = float)
-    self.R[0,0] = 2
-    self.R[1,1] = 30
+    self.Q[1,1] = 120
+    self.Q[2,2] = 1440
+    self.H = np.matrix([[1, 0, 0]], dtype = np.float32)
+    self.R = np.eye(1, dtype = np.float32)
+    self.R[0,0] = 4
+    #self.R[1,1] = 60
     #self.R[2,2] = 225
-    self.Zx = np.matrix([[0],[0]], dtype = float)
-    self.Zy = np.matrix([[0],[0]],  dtype = float)
-    self.del_t = 0.067
+    self.Zx = np.matrix([0], dtype = np.float32)
+    self.Zy = np.matrix([0],  dtype = np.float32)
+    self.del_t = 0.0
+    self.t = 0.0
 
+  def time_cb(self, info):
+    self.del_t = (float(info.data) - self.t) / 100
+    self.t = float(info.data)
+         
   def callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -90,29 +95,29 @@ class image_converter:
         fbp.position.y = bp.position.y
         self.init_flag = 1
     else:
-        self.A = np.matrix([[1, self.del_t, ((self.del_t**2)/2)],[0, 1, self.del_t], [0, 0, 1]], dtype=float)
+        self.A = np.matrix([[1, self.del_t, ((self.del_t**2)/2)],[0, 1, self.del_t], [0, 0, 1]], dtype=np.float32)
         
         self.X_prev = self.X
         self.X = np.matmul(self.A,self.X_prev)
         self.Px = np.matmul(np.matmul(self.A,self.Px),np.transpose(self.A)) + self.Q
         self.Zx[0] = bp.position.x
-        self.Zx[1] = (self.Zx[0] - self.X_prev[0])/self.del_t
+        #self.Zx[1] = (self.Zx[0] - self.X_prev[0])/self.del_t
         #self.Zx[2] = (self.Zx[1] - self.X_prev[1])/self.del_t
 
         self.gain_x = np.matmul(np.matmul(self.Px, np.transpose(self.H)), np.linalg.inv(np.matmul(np.matmul(self.H,self.Px),np.transpose(self.H)) + self.R))
         self.X = self.X + np.matmul(self.gain_x, (self.Zx - np.matmul(self.H, self.X)))
-        self.Px = np.matmul((np.eye(3,dtype=float)- np.matmul(self.gain_x,self.H)),self.Px)
+        self.Px = np.matmul((np.eye(3,dtype=np.float32)- np.matmul(self.gain_x,self.H)),self.Px)
         
         self.Y_prev = self.Y
         self.Y = np.matmul(self.A,self.Y_prev)
         self.Py = np.matmul(np.matmul(self.A,self.Py),np.transpose(self.A)) + self.Q
         self.Zy[0] = bp.position.y        
-        self.Zy[1] = (self.Zy[0] - self.Y_prev[0])/self.del_t
+        #self.Zy[1] = (self.Zy[0] - self.Y_prev[0])/self.del_t
         #self.Zy[2] = (self.Zy[1] - self.Y_prev[1])/self.del_t
         
         self.gain_y = np.matmul(np.matmul(self.Py, np.transpose(self.H)), np.linalg.inv(np.matmul(np.matmul(self.H,self.Py),np.transpose(self.H)) + self.R))
         self.Y = self.Y + np.matmul(self.gain_y, (self.Zy - np.matmul(self.H, self.Y)))
-        self.Py = np.matmul((np.eye(3,dtype=float)-np.matmul(self.gain_y,self.H)),self.Py)
+        self.Py = np.matmul((np.eye(3,dtype=np.float32)-np.matmul(self.gain_y,self.H)),self.Py)
         
         fbp.position.x = float(self.X.item(0))
         fbp.position.y = float(self.Y.item(0))
@@ -121,40 +126,13 @@ class image_converter:
         fba.linear.x = float(self.X.item(2))
         fba.linear.y = float(self.Y.item(2))
         
-        self.future_X = self.X
-        self.future_Y = self.Y
-        self.future_Px = self.Px
-        self.future_Py = self.Py
-
-        if(math.sqrt(self.X.item(1)**2 + self.Y.item(1)**2) > 3):
-          if(( (self.X.item(1)*self.X.item(2))+ (self.Y.item(1)*self.Y.item(2)) <0) ):
-            print('decelerating')
-            self.count = 0
-            while(math.sqrt(self.future_X.item(1)**2 + self.future_Y.item(1)**2) > 30):
-              self.count += 1
-              if(self.count > 400):
-                break
-              self.future_X = np.matmul(self.A, self.future_X)
-              self.future_Px = np.matmul(np.matmul(np.transpose(self.A), self.future_Px),self.A) + self.Q
-              self.future_Y = np.matmul(self.A, self.future_Y)
-              self.future_Py = np.matmul(np.matmul(np.transpose(self.A), self.future_Py),self.A) + self.Q
-              print('propagating, count = ', self.count)
-          fbs.position.x = self.future_X[0]
-          fbs.position.y = self.future_Y[0]
-        else:
-          fbs.position.x = self.X[0]
-          fbs.position.y = self.Y[0]
-        
-    self.radius = int(math.sqrt(self.future_Px.item(0)**2 + self.future_Py.item(0)**2))
-    cv2.ellipse(cv_image, (int(fbs.position.x)+320, 283-int(fbs.position.y)), (int(self.future_Px.item(0)),int(self.future_Py.item(0))),0,0,360,(0,0,255))
-    cv2.circle(cv_image, (int(fbs.position.x)+320, 283-int(fbs.position.y)), 8, (0,0,255), thickness=-1)
-
     try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
       self.kf_ballpub.publish(fbp)
       self.kf_ballpub2.publish(fbv)
       self.kf_ballpub3.publish(fba)
       self.kf_ballpub4.publish(fbs)
+      self.Px_pub.publish(np.reshape(self.Px, (9,1)))
+      self.Py_pub.publish(np.reshape(self.Py, (9,1)))
     except CvBridgeError as e:
       print(e)
 
--- src/predictstop.py
+++ src/predictstop.py
@@ -15,36 +15,55 @@ from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import CameraInfo
 from std_msgs.msg import Float64,Int32
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+
 class image_converter:
 
     def __init__(self):
         self.image_pub = rospy.Publisher("ball_prediction",Image,queue_size = 10)
-        rospy.Subscriber('ballposefiltered',Pose,self.bpos)
-        rospy.Subscriber('balltwistfiltered',Twist,self.btwis)
-        rospy.Subscriber('ballaccelfiltered',Twist,self.bacce)
+        rospy.Subscriber('/kf_ballpose',Pose, self.bpos)
+        rospy.Subscriber('/kf_ball_velocity',Twist, self.btwis)
+        rospy.Subscriber('/kf_ball_acceleration',Twist, self.bacce)
+        rospy.Subscriber('/ball_Px', numpy_msg(Floats), self.Px_cb)
+        rospy.Subscriber('/ball_Py', numpy_msg(Floats), self.Py_cb)
+        self.time_sub = rospy.Subscriber("/Time", Float64, self.time_cb)
         self.pts = []
-        self.x = 0
-        self.y = 0
-        self.xd = 0
-        self.yd = 0
-        self.xdd = 0
-        self.f = 0
-        self.ydd = 0
+        self.X = np.matrix([[0],[0], [0]], dtype = float)
+        self.Y = np.matrix([[0],[0], [0]], dtype = float)
+        self.Px = np.eye(3, dtype = float)
+        self.Py = np.eye(3, dtype = float)
+        self.Q = np.eye(3, dtype = float)
+        self.Q[0,0] = 1
+        self.Q[1,1] = 120
+        self.Q[2,2] = 1440
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/fieldroi",Image,self.callback)
+        self.del_t = 0.0
+        self.t = 0.0
 
+    def time_cb(self, info):
+        self.del_t = (float(info.data) - self.t) / 100
+        self.t = float(info.data)
+    
     def bpos(self,msg):
-        self.x = msg.position.x
-        self.y = msg.position.y
+        self.X[0] = msg.position.x
+        self.Y[0] = msg.position.y
 
     def btwis(self,msg):
-        self.xd = msg.linear.x
-        self.yd = msg.linear.y
+        self.X[1] = msg.linear.x
+        self.Y[1] = msg.linear.y
 
     def bacce(self,msg):
-        self.xdd = msg.linear.x
-        self.ydd = msg.linear.y
+        self.X[2] = msg.linear.x
+        self.Y[2] = msg.linear.y
 
+    def Px_cb(self,msg):
+        self.Px = np.reshape(msg.data, (3,3))
+
+
+    def Py_cb(self,msg):
+        self.Py = np.reshape(msg.data, (3,3))
 
     def callback(self,data):
         try:
@@ -52,20 +71,34 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        if self.xdd != 0 and self.ydd != 0 and self.xdd*self.xd <0 and self.ydd*self.yd<0:
-            x = self.x - self.xd**2/(2*self.xdd)
-            y = self.y - self.yd**2/(2*self.ydd)
-            cv2.circle(cv_image, (int(x)+320, 283-int(y)), 50 ,(0,0,255))
-            self.pts.append((int(x)+320, 283-int(y)))
-            self.f = 1
+        self.A = np.matrix([[1, self.del_t, ((self.del_t**2)/2)],[0, 1, self.del_t], [0, 0, 1]], dtype=float)
+        self.future_X = self.X
+        self.future_Y = self.Y
+        self.future_Px = self.Px
+        self.future_Py = self.Py
+
+        if(math.sqrt(self.X.item(1)**2 + self.Y.item(1)**2) > 3):
+          if(( (self.X.item(1)*self.X.item(2))+ (self.Y.item(1)*self.Y.item(2)) <0) ):
+            print('decelerating')
+            self.count = 0
+            while(math.sqrt(self.future_X.item(1)**2 + self.future_Y.item(1)**2) > 30):
+              self.count += 1
+              if(self.count > 200):
+                break
+              self.future_X = np.matmul(self.A, self.future_X)
+              self.future_Px = np.matmul(np.matmul(np.transpose(self.A), self.future_Px),self.A) + self.Q
+              self.future_Y = np.matmul(self.A, self.future_Y)
+              self.future_Py = np.matmul(np.matmul(np.transpose(self.A), self.future_Py),self.A) + self.Q
+              print('propagating, count = ', self.count)
+          self.x_stop = self.future_X[0]
+          self.y_stop = self.future_Y[0]
         else:
-            self.f = 0
-        for j in self.pts:
-            cv2.circle(cv_image, j, 2 ,(0,0,255),-1)
-        pts = np.array(self.pts)
-        pt = np.mean(pts,axis= 0)
-        print(pt)
-        cv2.circle(cv_image, (int(pt[0]),int(pt[1])), 2 ,(255,0,0),-1)
+          self.x_stop = self.X[0]
+          self.y_stop = self.Y[0]
+        
+        self.radius = int(math.sqrt(self.future_Px.item(0)**2 + self.future_Py.item(0)**2))
+        cv2.circle(cv_image, (int(self.x_stop)+320, 283-int(self.y_stop)), self.radius, 1)
+        cv2.circle(cv_image, (int(self.x_stop)+320, 283-int(self.y_stop)), 8, 1, thickness=-1)
         cv2.imshow("Image window", cv_image)
         cv2.waitKey(3)  
         try:
